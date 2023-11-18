6f4adf47502b02bb34c62c81b78ba041a66655f0
Sinane <sin.sim@outlook.fr>
2023-11-18 15:03:00 +0300

    verifying signatures

--- app/SplashView.tsx
+++ app/SplashView.tsx
@@ -1,21 +1,78 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   StyleSheet,
   StatusBar,
   Image,
   SafeAreaView,
   Dimensions,
+  Text,
 } from 'react-native';
-import {W3mButton} from '@web3modal/wagmi-react-native';
+import ConnectWalletButton from './components/ConnectWalletButton';
+import {useAccount, useSignMessage} from 'wagmi';
+import ActionButton from './components/ActionButton';
+import {recoverMessageAddress} from 'viem';
+
+function isSignatureVerified(verifiedAddresses: string[], address?: string) {
+  if (!address) {
+    return false;
+  }
+  return verifiedAddresses.includes(address);
+}
 
 function SplashView({}) {
+  const {isConnected, address} = useAccount();
+
+  const {
+    data: signMessageData,
+    error,
+    signMessage,
+    variables,
+  } = useSignMessage();
+  const [verifiedAddress, setVerifiedAddress] = useState<`0x${string}`[]>([]);
+
   const screenWidth = Dimensions.get('window').width;
   const screenHeight = Dimensions.get('window').height;
+
+  useEffect(() => {
+    async function verifiySignature() {
+      if (signMessageData && variables?.message) {
+        const recoveredAddress = await recoverMessageAddress({
+          message: variables?.message,
+          signature: signMessageData,
+        });
+
+        console.log('recoveredAddress', recoveredAddress);
+
+        setVerifiedAddress(prev => [...prev, recoveredAddress]);
+      }
+    }
+
+    verifiySignature();
+  }, [error, signMessageData, variables]);
+
+  useEffect(() => {
+    if (isSignatureVerified(verifiedAddress, address)) {
+      console.log('Signature verified');
+    }
+  }, [address, verifiedAddress]);
+
+  const handleSignMessage = () => {
+    signMessage({message: 'This is my wallet.'});
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" />
-      <W3mButton />
+      <ConnectWalletButton />
+      <Text style={styles.text}>
+        {isConnected ? 'Connected !' : 'Sign in with wallet connect'}
+      </Text>
+      {isConnected && (
+        <ActionButton text="Sign a message" onPress={handleSignMessage} />
+      )}
+      <Text style={styles.text}>
+        {isSignatureVerified(verifiedAddress, address) && 'Verified'}
+      </Text>
       <Image
         source={require('./assets/images/Cover.png')}
         resizeMode="contain"
@@ -29,6 +86,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'rgba(29,38,51,1)',
+    marginTop: StatusBar.currentHeight || 0,
   },
   actionButton: {
     textAlign: 'center',
@@ -36,6 +94,11 @@ const styles = StyleSheet.create({
     bottom: 0,
     alignSelf: 'center',
     marginBottom: 50,
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
