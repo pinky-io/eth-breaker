f52c1515a29ad3c308ed3e04ae704fc4fc520692
nada-r <nadarzer@gmail.com>
2023-11-18 20:42:35 +0100

    create raw content of user wallet activities

--- matching_engine/python/web3rosetta.py
+++ matching_engine/python/web3rosetta.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import csv
 
 with open('dapps_dictionary4.json', 'r') as file:
     valuable_dict = json.load(file)
@@ -28,4 +29,21 @@ for term in terms_list:
         valuable_key = translation_dict[key_part]
         translated_values[term] = valuable_dict.get(valuable_key, "Not found")
 
-print(translated_values)
+#print(translated_values)
+
+with open('output.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(translated_values)
+    for key, value in valuable_dict.items():
+        writer.writerow([key, value])
+
+terms_list.reverse()
+
+texts_to_combine = []
+for name in terms_list:
+    if name in translated_values:
+        texts_to_combine.append(translated_values[name])
+
+combined_text = ' '.join(texts_to_combine)
+
+print(combined_text)
